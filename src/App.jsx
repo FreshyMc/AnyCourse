9250348c1f409ebf28f0518243df2e461432c2d3
FreshyMc <12915203+FreshyMc@users.noreply.github.com>
2025-03-14 20:57:01 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Navigation from './components/Navigation'
+
+import './assets/style.css';
+
+function App() {
+  return (
+    <Navigation />
+  )
+}
+
+export default App
